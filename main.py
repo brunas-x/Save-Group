39bf9803e393d556a6f75266f90ba38f12b78176
brunas-x <87294624+brunas-x@users.noreply.github.com>
2022-01-16 09:48:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ async def start_handler(bot: Client, event: Message):
         except (Error, UnicodeDecodeError):
             file_id = int(__data)
         try:
-            await bot.forward_messages(chat_id=event.chat.id, from_chat_id=Config.DB_CHANNEL_ID, message_ids=file_id)
+            await bot.copy_message(chat_id=event.chat.id, from_chat_id=Config.DB_CHANNEL_ID, message_ids=file_id)
         except:
             await sendMessage(bot, "Unable to Get Message!\n\nReport at @DevsZone !!", event.message_id, event.chat.id)
 
